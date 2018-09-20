8d67614ed09d277259a75d7fc1f8906d854d3f8f
DESKTOP-TBD5BHF\DT_win10 <s890533@gmail.com>
2018-09-20 13:50:06 +0800

    完成

---
+++ CannotSleep/CannotSleep/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace CannotSleep
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(0, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Can Sleep";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(150, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(31, 26);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(119, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(31, 26);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "_";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(181, 64);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ CannotSleep/CannotSleep/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CannotSleep
+{
+    public partial class Form1 : Form
+    {
+
+        private int curr_x;
+        private int curr_y;
+        private int mouse_x;
+        private int mouse_y;
+        private bool isWinMove;
+        bool IsCannotSleep;
+        int mousecount;
+
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte bVK, byte bScan,uint dwFlags,int dw);
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+            timer1.Start();
+            timer1.Tick += new EventHandler(CannotSleep);
+            IsCannotSleep = false;
+        }
+
+        private void CannotSleep(object sender,EventArgs e)
+        {
+            Point position = MousePosition;
+            if(position.X == this.mouse_x && position.Y == this.mouse_y)
+            {
+                mousecount++;
+            }
+            else
+            {
+                mousecount = 0;
+                this.mouse_x = position.X;
+                this.mouse_y = position.Y;
+
+            }
+            if(mousecount > 120)
+            {
+                if (IsCannotSleep)
+                {
+                    keybd_event(0x12, 0, 0, 0);
+                    keybd_event(0x12, 0, 0x0002, 0);
+                    mousecount = 0;
+                }
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Can Sleep")
+            {
+                button1.Text = "Cannot Sleep";
+                IsCannotSleep = true;
+                timer1.Enabled = true;
+                button1.BackColor = Color.Salmon;
+
+            }
+            else
+            {
+                button1.Text = "Can Sleep";
+                IsCannotSleep = false;
+                timer1.Enabled = false;
+                button1.BackColor = Color.CornflowerBlue;
+            }
+        }
+
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            this.TopMost = true;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.curr_x = e.X;
+                this.curr_y = e.Y;
+                this.isWinMove = true;
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.isWinMove == true)
+            {
+                this.Location = new Point(this.Left + e.X - this.curr_x, this.Top + e.Y - this.curr_y);
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.isWinMove = false;
+        }
+    }
+}
